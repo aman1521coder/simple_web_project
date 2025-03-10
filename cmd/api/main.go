39a554c1af678a2b90a8a56dc17c3e712586d161
aman1521coder <amanesu1521@gmail.com>
2025-03-10 11:16:34 +0300

    database config done

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,12 +4,15 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"context" // New import
+"database/sql" // New import
 
 	
 	 "time"
 	"flag"
 	
 	"log"
+	_ "github.com/lib/pq"
 )
 
 // verision of our application
@@ -19,17 +22,31 @@ const version="1.0.0"
 type config  struct{
  port int
  env string
+ db struct {
+	dsn string
+ }
 }
 type application struct{
 	config config
 	log *log.Logger
 }
+
 func main() {
 	var conf config
 	flag.IntVar(&conf.port,"port",4000,"port number for our port")
 	flag.StringVar(&conf.env,"env","development", "Environment (development|staging|production)")
+flag.StringVar(&conf.db.dsn ,"db-dsn",os.Getenv("MOVIE_GO"),"PostgreSQL DSN")
 	flag.Parse()
+	
 	logger := log.New(os.Stdout, "", log.Ldate | log.Ltime)
+	db ,err:=openDb(conf)
+	if err!=nil{
+		log.Fatal(err)
+	}
+
+	defer db.Close()
+	logger.Printf("database connection pool established ")
+	
 	app:=&application{
 		config:conf,
 
@@ -44,10 +61,24 @@ ReadTimeout: 10 * time.Second,
 WriteTimeout: 30 * time.Second,
 	}
 	logger.Printf("starting %s server on %s", conf.env, srv.Addr)
-err := srv.ListenAndServe()
+err = srv.ListenAndServe()
 if err !=nil{
 	logger.Fatal(err)
 }
 
 
+}
+func openDb(conf config)(*sql.DB,error){
+	db ,err:=sql.Open("postgres",conf.db.dsn)
+	if err !=nil{
+		return nil ,err
+	}
+	ctx,cancel:=context.WithTimeout(context.Background(),5*time.Second)
+	defer cancel()
+	err=db.PingContext(ctx)
+	if err!=nil{
+ return nil,err
+
+	}
+	return db,nil
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/aman1521coder/simple_project
 
 go 1.24.0
 
-require github.com/julienschmidt/httprouter v1.3.0 // indirect
+require (
+	github.com/julienschmidt/httprouter v1.3.0 // indirect
+	github.com/lib/pq v1.10.0 // indirect
+)
